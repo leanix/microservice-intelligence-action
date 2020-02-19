7833c05b087b029eb9f9504adf05a8dc2fc766e7
Maik Schwan <maik.schwan@leanix.net>
2020-02-19 14:06:34 +0100

    HOOK-1330 Fix action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
 set -e
+
+# Ensure JAVA_HOME is set correctly even if the variable was passed in during docker run.
+# If the calling github workflow uses the setup-java action it will set the env var and pass it into this container.
+export JAVA_HOME="/docker-java-home/jre"
 
 case $1 in
     mvn)
-        export CMD="mvn org.codehaus.mojo:license-maven-plugin:download-licenses -U"
+        export CMD="mvn -B org.codehaus.mojo:license-maven-plugin:download-licenses -U"
         ;;
     npm)
         export CMD="npm-license-checker"
         ;;
 esac
 
+cd /github/workspace
 eval $CMD
-java -jar /pivio.jar -serviceurl "https://int.leanix.net/services/integrations/v2/pivio/document" -addfield "api_token=${INT_LEANIX_NET_MICROSERVICES_API_TOKEN}"
+exec java -jar /pivio.jar -serviceurl "https://int.leanix.net/services/integrations/v2/pivio/document" -addfield "api_token=${INT_LEANIX_NET_MICROSERVICES_API_TOKEN}" -uploadfailexit1 -verbose
