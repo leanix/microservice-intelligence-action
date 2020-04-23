dc14a0b627318c64084912ad2a7996460bd786e2
Fabian Böller <fabian.boeller@leanix.net>
2020-04-23 12:54:59 +0200

    INTG-681 Filter PRs

--- lead-time/new_index.js
+++ lead-time/new_index.js
@@ -5,6 +5,10 @@ const myHeaders = {
     'Authorization': "token " + process.env.GITHUB_API_TOKEN
 };
 
-fetch("https://api.github.com/repos/leanix/camunda/pulls", { headers: myHeaders })
+// The maximum page size is 100
+fetch("https://api.github.com/repos/leanix/camunda/pulls?state=closed&base=master&per_page=100", { headers: myHeaders })
     .then(response => response.json())
-    .then(response => console.log(response));
+    .then(response => {
+        console.log(response);
+        console.log(response.length);
+    });
