9f8b5ecb531df9394510c9bd41f5db8170e28208
Fabian Böller <fabian.boeller@leanix.net>
2020-04-23 16:49:28 +0200

    INTG-681 Send stuff to metrics

--- lead-time/new_index.js
+++ lead-time/new_index.js
@@ -1,14 +1,56 @@
 
 const fetch = require('node-fetch');
+const request = require('request-promise-native');
 
 const myHeaders = {
     'Authorization': "token " + process.env.GITHUB_API_TOKEN
 };
 
+const API_TOKEN = 'YayamXVBL4RChnKEBYzGNjSLrYBdrnCagrrdEgW5';
+const DOMAIN = 'https://test-app-flow-2.leanix.net';
+const WORKSPACE_ID = '120d4a91-ece8-4888-9409-92400cc31a44';
+
+async function getAccessToken(apiToken) {
+    try {
+        const result = await request.post({
+            url: `${DOMAIN}/services/mtm/v1/oauth2/token`,
+            auth: {
+                user: 'apitoken',
+                password: apiToken
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials',
+            json: true
+        });
+        return result.access_token;
+    } catch (error) {
+        console.log('Error while fetching access token:', error);
+        process.exit(1);
+    }
+}
+
+async function sendMetrics(accessToken, metricsPoint) {
+    try {
+        return request.post({
+            url: `${DOMAIN}/services/metrics/v1/points`,
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            body: metricsPoint,
+            json: true
+        });
+    } catch (error) {
+        console.log('Error sending metrics:', error, 'for point ', metricsPoint);
+        process.exit(1);
+    }
+}
+
 // TODO Handle develop
 // The maximum page size is 100
 async function fetchLeadTimes(repo) {
-    return fetch("https://api.github.com/repos/leanix/" + repo.name + "/pulls?state=closed&base=" + repo.baseBranch + "&per_page=10", { headers: myHeaders })
+    return fetch("https://api.github.com/repos/leanix/" + repo.name + "/pulls?state=closed&base=" + repo.baseBranch + "&per_page=100", { headers: myHeaders })
         .then(response => response.json())
         .then(prs =>
             Promise.all(prs.filter(pr => pr.merged_at)
@@ -34,6 +76,24 @@ const repos = [
     { name: 'leanix-pathfinder', baseBranch: 'develop' }
 ];
 
+function createMetricsPoint(repo, metric, value, timeStamp) {
+    return {
+        measurement: "leadtime_" + metric,
+        workspaceId: WORKSPACE_ID,
+        time: new Date(timeStamp).toISOString(),
+        tags: [{
+            k: 'externalIdProject',
+            v: repo
+        }],
+        fields: [
+            {
+                k: repo,
+                v: value
+            }
+        ]
+    };
+}
+
 async function main() {
     const prOpenTimes = await Promise.all(repos.map(repo => fetchLeadTimes(repo)));
     const untilReleaseTimes = await Promise.all(prOpenTimes
@@ -58,6 +118,20 @@ async function main() {
         ));
     console.log(prOpenTimes);
     console.log(untilReleaseTimes);
+
+    const accessToken = await getAccessToken(API_TOKEN);
+    for (const prOpenTime of prOpenTimes) {
+        const responses = await Promise.all(prOpenTime
+            .map(r => createMetricsPoint(r.repository, 'branchLifeTime', r.durationMin, r.merged_at))
+            .map(metricsPoint => sendMetrics(accessToken, metricsPoint))
+        );
+    }
+    for (const untilReleaseTime of untilReleaseTimes) {
+        const responses = await Promise.all(untilReleaseTime
+            .map(r => createMetricsPoint(r.repository, 'mergeUntilReleaseTime', r.untilReleaseMin, r.merged_at))
+            .map(metricsPoint => sendMetrics(accessToken, metricsPoint))
+        );
+    }
 }
 
 main();
